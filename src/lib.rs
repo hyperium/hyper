a3fc51611f31182190bef36d8df38c4df70a93b0
Stanislav Panferov <fnight.m@gmail.com>
2014-10-08 11:38:01 +0400

    Add cookie_rs as default feature and implement conditional compilation.

--- src/lib.rs
+++ src/lib.rs
@@ -136,7 +136,7 @@ extern crate "unsafe-any" as uany;
 extern crate "move-acceptor" as macceptor;
 extern crate intertwine;
 extern crate typeable;
-extern crate cookie;
+#[cfg(feature = "cookie_rs")] extern crate cookie;
 
 pub use std::io::net::ip::{SocketAddr, IpAddr, Ipv4Addr, Ipv6Addr, Port};
 pub use mimewrapper::mime;
