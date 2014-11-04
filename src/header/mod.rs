14687ad6d22626070595b2716e28cb9da08ffbe5
Jamie Turner <jamwt@dropbox.com>
2014-11-03 21:06:30 -0800

    Re-add len() and clear() as inherent Headers methods.

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -268,6 +268,16 @@ impl Headers {
             inner: self.data.iter()
         }
     }
+
+    /// Returns the number of headers in the map.
+    pub fn len(&self) -> uint {
+        self.data.len()
+    }
+
+    /// Remove all headers from the map.
+    pub fn clear(&mut self) {
+        self.data.clear()
+    }
 }
 
 impl fmt::Show for Headers {
@@ -515,6 +525,25 @@ mod tests {
         headers.set_raw("content-LENGTH", vec![b"20".to_vec()]);
         assert_eq!(headers.get_raw("Content-length").unwrap(), [b"20".to_vec()][]);
         assert_eq!(headers.get(), Some(&ContentLength(20)));
+    }
+
+    #[test]
+    fn test_len() {
+        let mut headers = Headers::new();
+        headers.set(ContentLength(10));
+        assert_eq!(headers.len(), 1);
+        headers.set(ContentType(Mime(Text, Plain, vec![])));
+        assert_eq!(headers.len(), 2);
+    }
+
+    #[test]
+    fn test_clear() {
+        let mut headers = Headers::new();
+        headers.set(ContentLength(10));
+        headers.set(ContentType(Mime(Text, Plain, vec![])));
+        assert_eq!(headers.len(), 2);
+        headers.clear();
+        assert_eq!(headers.len(), 0);
     }
 }
 
