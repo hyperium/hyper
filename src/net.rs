3bd1ac5c28c6a40a99c1a5f0ee78b1c53a615f11
Kelvin Ly <kelvin.ly1618@gmail.com>
2014-12-17 18:00:47 -0500

    Working TLS implementation

--- src/net.rs
+++ src/net.rs
@@ -191,6 +191,7 @@ impl NetworkListener<HttpStream, HttpAcceptor> for HttpListener {
             return Err(lift_ssl_error(err));
         }
         ssl_context.set_verify(SslVerifyNone, None);
+        ssl_context.set_verify_depth(1);
         Ok(HttpsL(try!(TcpListener::bind(addr)), ssl_context))
     }
 
