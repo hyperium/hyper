1088f8f4e2b121260fa1a87d5dff60e2cb040605
Kelvin Ly <kelvin.ly1618@gmail.com>
2014-12-24 00:01:30 -0500

    It compiles!

--- src/net.rs
+++ src/net.rs
@@ -175,14 +175,15 @@ impl NetworkListener<HttpStream, HttpAcceptor> for HttpListener {
     }
 }
 
-// A wrapper to layer SSL/TLS on top of another listener
+/// A wrapper to layer SSL/TLS on top of another listener
 #[deriving(Clone)]
 pub struct TlsListener<L> {
     inner: L,
     context: Option<Arc<SslContext>>
 }
 
-impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send + Clone> Listener<S, TlsAcceptor<A>> for TlsListener<L> {
+impl <S, A, L> Listener<TlsStream<S>, TlsAcceptor<A>> for TlsListener<L> where
+        S: NetworkStream + Send + Clone, A: NetworkAcceptor<S>, L: NetworkListener<S, A> {
     #[inline]
     fn listen(self) -> IoResult<TlsAcceptor<A>> {
         match self.context {
@@ -195,8 +196,10 @@ impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send +
     }
 }
 
-impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send + Clone> NetworkListener<S, TlsAcceptor<A>> for TlsListener<L> {
+impl <S, A, L> NetworkListener<TlsStream<S>, TlsAcceptor<A>> for TlsListener<L> where
+        S: NetworkStream + Send + Clone, A: NetworkAcceptor<S>, L: NetworkListener<S, A> {
     #[inline]
+    #[allow(unused_variables)]
     fn bind<To: ToSocketAddr>(addr: To) -> IoResult<TlsListener<L>> {
         panic!("Binding on TLS listener without a context is forbidden");
     }
@@ -207,9 +210,10 @@ impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send +
     }
 }
 
-impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send + Clone> TlsListener<L> {
+impl <L: NetworkListener<S, A>, S: NetworkStream + Send + Clone, A: NetworkAcceptor<S>> TlsListener<L> {
+    /// Binds a listener to given address and layers the TLS protocol over the resulting stream.
     #[inline]
-    fn bind_with_ssl<To: ToSocketAddr>(addr: To, cert: Path, key: Path) -> IoResult<TlsListener<L>> {
+    pub fn bind_with_ssl<To: ToSocketAddr>(addr: To, cert: Path, key: Path) -> IoResult<TlsListener<L>> {
         let mut ssl_context = try!(SslContext::new(Sslv23).map_err(lift_ssl_error));
         if let Some(err) = ssl_context.set_cipher_list("DEFAULT") {
             return Err(lift_ssl_error(err));
@@ -222,7 +226,7 @@ impl <L: NetworkListener<S, A>, A: NetworkAcceptor<S>, S: NetworkStream + Send +
         }
         ssl_context.set_verify(SslVerifyNone, None);
         Ok(TlsListener {
-            inner: try!(NetworkListener::<L>::bind(addr)),
+            inner: try!(NetworkListener::<S, A>::bind(addr)),
             context: Some(Arc::<SslContext>::new(ssl_context))
         })
     }
@@ -248,31 +252,35 @@ impl NetworkAcceptor<HttpStream> for HttpAcceptor {
     }
 }
 
+/// Converts a normal stream acceptor into a TLS acceptor by layering a TLS connection over it
 #[deriving(Clone)]
 pub struct TlsAcceptor<A> {
     inner: A,
     context: Arc<SslContext>
 }
 
-impl <A: NetworkAcceptor<S>, S: NetworkStream + Send + Clone> Acceptor<TlsStream<S>> for TlsAcceptor<A> {
+impl <S, A> Acceptor<TlsStream<S>> for TlsAcceptor<A> where
+        S: NetworkStream + Send + Clone, A: NetworkAcceptor<S> {
     #[inline]
     fn accept(&mut self) -> IoResult<TlsStream<S>> {
         let stream = try!(self.inner.accept());
-        let ssl_stream = try!(SslStream::<S>::new_server(&**self.context, stream).
+        let ssl_stream = try!(SslStream::<S>::new_server(&*self.context, stream).
                              map_err(lift_ssl_error));
-        TlsStream {
+        Ok(TlsStream {
             inner: ssl_stream
-        }
+        })
     }
 }
 
-impl <A: NetworkAcceptor<S>, S: NetworkStream + Send + Clone> NetworkAcceptor<TlsStream<S>> for TlsAcceptor<A> {
+impl <S, A> NetworkAcceptor<TlsStream<S>> for TlsAcceptor<A> where
+        S: NetworkStream + Send + Clone, A: NetworkAcceptor<S> {
     #[inline]
     fn close(&mut self) -> IoResult<()> {
-        self.inner.close_accept()
+        self.inner.close()
     }
 }
 
+/// Layers a TLS connection over an arbitrary stream
 #[deriving(Clone)]
 pub struct TlsStream<S> {
     inner: SslStream<S>
