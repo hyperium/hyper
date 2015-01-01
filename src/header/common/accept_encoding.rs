1c82f566b486350c684825f806161559d5df0bee
Pyfisch <pyfisch@gmail.com>
2015-01-01 14:03:13 +0100

    QualityValue -> QualityItem, added ::new() and qitem()
    * Renamed QualityValue to QualityItem
    * Added ::new() to QualityItem
    * Added function qitem() to create QualityItem with q=1.0

--- src/header/common/accept_encoding.rs
+++ src/header/common/accept_encoding.rs
@@ -8,9 +8,9 @@ use header::shared;
 /// The `Accept-Encoding` header can be used by clients to indicate what
 /// response encodings they accept.
 #[deriving(Clone, PartialEq, Show)]
-pub struct AcceptEncoding(pub Vec<shared::QualityValue<shared::Encoding>>);
+pub struct AcceptEncoding(pub Vec<shared::QualityItem<shared::Encoding>>);
 
-deref!(AcceptEncoding -> Vec<shared::QualityValue<shared::Encoding>>);
+deref!(AcceptEncoding -> Vec<shared::QualityItem<shared::Encoding>>);
 
 impl header::Header for AcceptEncoding {
     fn header_name(_: Option<AcceptEncoding>) -> &'static str {
@@ -32,8 +32,8 @@ impl header::HeaderFormat for AcceptEncoding {
 fn test_parse_header() {
     let a: AcceptEncoding = header::Header::parse_header([b"gzip;q=1.0, identity; q=0.5".to_vec()].as_slice()).unwrap();
     let b = AcceptEncoding(vec![
-        shared::QualityValue{value: shared::Gzip, quality: 1f32},
-        shared::QualityValue{value: shared::Identity, quality: 0.5f32},
+        shared::QualityItem{item: shared::Gzip, quality: 1f32},
+        shared::QualityItem{item: shared::Identity, quality: 0.5f32},
     ]);
     assert_eq!(a, b);
 }
