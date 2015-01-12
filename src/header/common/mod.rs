bd49e43957bc42bf7f57fcf7110f76e168641acf
Pyfisch <pyfisch@gmail.com>
2015-01-12 20:53:55 +0100

    refactor(headers): add header macros and add two example uses
    Add `impl_header!` and `impl_list_header!` macros. Rewrite `Accept-Encoding`
    and `Server` header to use the new macros.

--- src/header/common/mod.rs
+++ src/header/common/mod.rs
@@ -76,6 +76,51 @@ macro_rules! deref(
     }
 );
 
+macro_rules! impl_list_header(
+    ($from:ident, $name:expr, $item:ty) => {
+        deref!($from => $item);
+
+        impl header::Header for $from {
+            fn header_name(_: Option<$from>) -> &'static str {
+                $name
+            }
+
+            fn parse_header(raw: &[Vec<u8>]) -> Option<$from> {
+                shared::from_comma_delimited(raw).map($from)
+            }
+        }
+
+        impl header::HeaderFormat for $from {
+            fn fmt_header(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+                shared::fmt_comma_delimited(fmt, &self[])
+            }
+        }
+    }
+);
+
+macro_rules! impl_header(
+    ($from:ident, $name:expr, $item:ty) => {
+        deref!($from => $item);
+
+        impl header::Header for $from {
+            fn header_name(_: Option<$from>) -> &'static str {
+                $name
+            }
+
+            fn parse_header(raw: &[Vec<u8>]) -> Option<$from> {
+                from_one_raw_str(raw).map($from)
+            }
+        }
+
+        impl header::HeaderFormat for $from {
+            fn fmt_header(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+                let value = &(self[]);
+                value.fmt(fmt)
+            }
+        }
+    }
+);
+
 /// Exposes the AccessControl* family of headers.
 pub mod access_control;
 
--- src/header/common/server.rs
+++ src/header/common/server.rs
@@ -1,4 +1,4 @@
-use header::{Header, HeaderFormat};
+use header;
 use std::fmt::{self, Show};
 use header::shared::util::from_one_raw_str;
 
@@ -8,23 +8,8 @@ use header::shared::util::from_one_raw_str;
 #[derive(Clone, PartialEq, Show)]
 pub struct Server(pub String);
 
-deref!(Server => String);
-
-impl Header for Server {
-    fn header_name(_: Option<Server>) -> &'static str {
-        "Server"
-    }
-
-    fn parse_header(raw: &[Vec<u8>]) -> Option<Server> {
-        from_one_raw_str(raw).map(|s| Server(s))
-    }
-}
-
-impl HeaderFormat for Server {
-    fn fmt_header(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        let Server(ref value) = *self;
-        value.fmt(fmt)
-    }
-}
+impl_header!(Server,
+             "Server",
+             String);
 
 bench_header!(bench, Server, { vec![b"Some String".to_vec()] });
