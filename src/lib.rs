e24b1ba1996e066c199a64f67b2b8984e6eee161
Sean McArthur <sean.monstar@gmail.com>
2017-06-06 12:36:46 -0700

    v0.10.12

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://hyper.rs/hyper/v0.10.11")]
+#![doc(html_root_url = "https://docs.rs/hyper/v0.10.12")]
 #![cfg_attr(test, deny(missing_docs))]
 #![cfg_attr(test, deny(warnings))]
 #![cfg_attr(all(test, feature = "nightly"), feature(test))]
