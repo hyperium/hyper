02a9c29e2e816c8a583f65b372fcf7b8503e6bad
Hrvoje Ban <hban@users.noreply.github.com>
2018-07-08 10:11:38 +0200

    feat(client): implement rfc 6555 (happy eyeballs)
    Update client connector to attempt a parallel connection using
    alternative address family, if connection using preferred address family
    takes too long.
    
    Closes: #1316

--- src/client/dns.rs
+++ src/client/dns.rs
@@ -35,6 +35,10 @@ pub struct IpAddrs {
 }
 
 impl IpAddrs {
+    pub fn new(addrs: Vec<SocketAddr>) -> Self {
+        IpAddrs { iter: addrs.into_iter() }
+    }
+
     pub fn try_parse(host: &str, port: u16) -> Option<IpAddrs> {
         if let Ok(addr) = host.parse::<Ipv4Addr>() {
             let addr = SocketAddrV4::new(addr, port);
@@ -46,6 +50,23 @@ impl IpAddrs {
         }
         None
     }
+
+    pub fn split_by_preference(self) -> (IpAddrs, IpAddrs) {
+        let preferring_v6 = self.iter
+            .as_slice()
+            .first()
+            .map(SocketAddr::is_ipv6)
+            .unwrap_or(false);
+
+        let (preferred, fallback) = self.iter
+            .partition::<Vec<_>, _>(|addr| addr.is_ipv6() == preferring_v6);
+
+        (IpAddrs::new(preferred), IpAddrs::new(fallback))
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.iter.as_slice().is_empty()
+    }
 }
 
 impl Iterator for IpAddrs {
@@ -53,5 +74,27 @@ impl Iterator for IpAddrs {
     #[inline]
     fn next(&mut self) -> Option<SocketAddr> {
         self.iter.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{Ipv4Addr, Ipv6Addr};
+    use super::*;
+
+    #[test]
+    fn test_ip_addrs_split_by_preference() {
+        let v4_addr = (Ipv4Addr::new(127, 0, 0, 1), 80).into();
+        let v6_addr = (Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1), 80).into();
+
+        let (mut preferred, mut fallback) =
+            IpAddrs { iter: vec![v4_addr, v6_addr].into_iter() }.split_by_preference();
+        assert!(preferred.next().unwrap().is_ipv4());
+        assert!(fallback.next().unwrap().is_ipv6());
+
+        let (mut preferred, mut fallback) =
+            IpAddrs { iter: vec![v6_addr, v4_addr].into_iter() }.split_by_preference();
+        assert!(preferred.next().unwrap().is_ipv6());
+        assert!(fallback.next().unwrap().is_ipv4());
     }
 }
