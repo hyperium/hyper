aaaa7d5e62982c4c2040cb398022968e3f3d96e6
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-03-24 01:01:34 +0300

    feat(client) Add client::conn::Connection::without_shutdown
    Returns wrapper Future instance which allows
    to use `poll_without_shutdown` method
    more ergonomically.

--- src/client/conn.rs
+++ src/client/conn.rs
@@ -386,6 +386,16 @@ where
             }
         }
     }
+
+    /// Prevent shutdown of the underlying IO object at the end of service the request,
+    /// instead run `into_parts`. This is a convenience wrapper over `poll_without_shutdown`.
+    pub fn without_shutdown(self) -> impl Future<Item=Parts<T>, Error=::Error> {
+        let mut conn = Some(self);
+        futures::future::poll_fn(move || -> ::Result<_> {
+            try_ready!(conn.as_mut().unwrap().poll_without_shutdown());
+            Ok(conn.take().unwrap().into_parts().into())
+        })
+    }
 }
 
 impl<T, B> Future for Connection<T, B>
