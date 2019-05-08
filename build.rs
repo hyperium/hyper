4dd18a8a07a3deb6c23b3d1d37fbab0f9bf77487
M3rs <morandyj@gmail.com>
2019-05-08 18:57:19 -0500

    feat(client): Implement TryFrom for Destination
    Add TryFrom<Uri> impl for Destination, for compiler version >= 1.34.
    Add basic unit tests for TryFrom impl
    
    Ref Issue: Implement TryFrom for Destination #1808

--- build.rs
+++ build.rs
@@ -3,7 +3,11 @@ extern crate rustc_version;
 use rustc_version::{version, Version};
 
 fn main() {
-    if version().unwrap() >= Version::parse("1.30.0").unwrap() {
+    let version = version().unwrap();
+    if version >= Version::parse("1.30.0").unwrap() {
         println!("cargo:rustc-cfg=error_source");
+    }
+    if version >= Version::parse("1.34.0").unwrap() {
+        println!("cargo:rustc-cfg=try_from");
     }
 }
