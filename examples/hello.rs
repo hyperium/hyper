327fc39e90c7bfe0115c761d69891dc5fa3e14f9
Fuyang Liu <fuyangl@spotify.com>
2019-07-10 16:07:47 +0200

    update the example hello

--- examples/hello.rs
+++ examples/hello.rs
@@ -5,19 +5,21 @@ extern crate pretty_env_logger;
 
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::rt;
 
 async fn hello(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     Ok(Response::new(Body::from("Hello World!")))
 }
 
-async fn serve() {
+#[tokio::main]
+pub async fn main() {
+    pretty_env_logger::init();
+
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let server = Server::bind(&addr)
         .serve(make_service_fn(|_| {
             // This is the `Service` that will handle the connection.
-            // `service_fn_ok` is a helper to convert a function that
+            // `service_fn` is a helper to convert a function that
             // returns a Response into a `Service`.
             async {
                 Ok::<_, hyper::Error>(service_fn(hello))
@@ -29,10 +31,4 @@ async fn serve() {
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
-}
-
-fn main() {
-    pretty_env_logger::init();
-
-    rt::run(serve());
 }
