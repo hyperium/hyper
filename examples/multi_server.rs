19ba891fac8c46139a34c30604213864e5cafed0
messense <messense@icloud.com>
2019-07-12 15:40:14 +0800

    docs(examples): Update multi_server example to use async await

--- examples/multi_server.rs
+++ examples/multi_server.rs
@@ -1,34 +1,47 @@
+#![feature(async_await)]
 #![deny(warnings)]
 extern crate hyper;
 extern crate pretty_env_logger;
 
-use hyper::{Body, Response, Server};
-use hyper::service::service_fn_ok;
-use hyper::rt::{self, Future};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{service_fn, make_service_fn};
+use futures_util::future::join;
 
 static INDEX1: &'static [u8] = b"The 1st service!";
 static INDEX2: &'static [u8] = b"The 2nd service!";
 
-fn main() {
+async fn index1(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    Ok(Response::new(Body::from(INDEX1)))
+}
+
+async fn index2(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    Ok(Response::new(Body::from(INDEX2)))
+}
+
+#[hyper::rt::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     pretty_env_logger::init();
 
     let addr1 = ([127, 0, 0, 1], 1337).into();
     let addr2 = ([127, 0, 0, 1], 1338).into();
 
-    rt::run(rt::lazy(move || {
-        let srv1 = Server::bind(&addr1)
-            .serve(|| service_fn_ok(|_| Response::new(Body::from(INDEX1))))
-            .map_err(|e| eprintln!("server 1 error: {}", e));
+    let srv1 = Server::bind(&addr1)
+        .serve(make_service_fn(|_| {
+            async {
+                Ok::<_, hyper::Error>(service_fn(index1))
+            }
+        }));
 
-        let srv2 = Server::bind(&addr2)
-            .serve(|| service_fn_ok(|_| Response::new(Body::from(INDEX2))))
-            .map_err(|e| eprintln!("server 2 error: {}", e));
+    let srv2 = Server::bind(&addr2)
+        .serve(make_service_fn(|_| {
+            async {
+                Ok::<_, hyper::Error>(service_fn(index2))
+            }
+        }));
 
-        println!("Listening on http://{} and http://{}", addr1, addr2);
+    println!("Listening on http://{} and http://{}", addr1, addr2);
 
-        rt::spawn(srv1);
-        rt::spawn(srv2);
+    let _ret = join(srv1, srv2).await;
 
-        Ok(())
-    }));
+    Ok(())
 }
