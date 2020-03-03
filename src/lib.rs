5b046a1f8fc666ca64b833174c5eae34e6686838
Sean McArthur <sean@seanmonstar.com>
2020-03-03 15:24:47 -0800

    v0.13.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/hyper/0.13.2")]
+#![doc(html_root_url = "https://docs.rs/hyper/0.13.3")]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(rust_2018_idioms))]
