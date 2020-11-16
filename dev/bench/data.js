bedf56da85c2b130a5d50756fc190ccf33343020
github-action-benchmark <github@users.noreply.github.com>
2020-11-16 22:34:48 +0000

    add connect (cargo) benchmark result for 0fd7d3c3635fa5a6d2d9e9e9471ae43ca3f09cad

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1605566088045,
+  "repoUrl": "https://github.com/hyperium/hyper",
+  "entries": {
+    "connect": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "0fd7d3c3635fa5a6d2d9e9e9471ae43ca3f09cad",
+          "message": "fixup benchmark output.txt",
+          "timestamp": "2020-11-16T14:31:59-08:00",
+          "tree_id": "4a3e2b0368c81605beafc6f4da7026519a929f19",
+          "url": "https://github.com/hyperium/hyper/commit/0fd7d3c3635fa5a6d2d9e9e9471ae43ca3f09cad"
+        },
+        "date": 1605566086676,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 39056,
+            "range": "± 8021",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
