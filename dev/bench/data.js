991b41c81e787e471c100a67104e5470283dc4e9
github-action-benchmark <github@users.noreply.github.com>
2020-12-21 20:27:57 +0000

    add connect (cargo) benchmark result for 6c593c292508f31a2986d5ddac3fb23b234a0e2c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608071299841,
+  "lastUpdate": 1608582477587,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -659,6 +659,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 48430,
             "range": "± 4431",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "46450720+bishtpawan@users.noreply.github.com",
+            "name": "Pawan Singh Bisht",
+            "username": "bishtpawan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c593c292508f31a2986d5ddac3fb23b234a0e2c",
+          "message": "refactor(error): use `matches!` macro in `Error::is_*` methods (#2367)",
+          "timestamp": "2020-12-21T12:25:47-08:00",
+          "tree_id": "c61537b497a46b23e64414fa1f2c822dadd22fc4",
+          "url": "https://github.com/hyperium/hyper/commit/6c593c292508f31a2986d5ddac3fb23b234a0e2c"
+        },
+        "date": 1608582476207,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 43349,
+            "range": "± 10967",
             "unit": "ns/iter"
           }
         ]
