b8e3e299bfa233e32de7dcb821824ddcfd42a269
github-action-benchmark <github@users.noreply.github.com>
2021-01-13 00:20:49 +0000

    add connect (cargo) benchmark result for a15f3f7f0f536c74d51636bbc00f6b5ec110472b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610491838452,
+  "lastUpdate": 1610497249079,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -1019,6 +1019,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 44797,
             "range": "± 2546",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.westerlind@distilnetworks.com",
+            "name": "Markus Westerlind",
+            "username": "Marwes"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a15f3f7f0f536c74d51636bbc00f6b5ec110472b",
+          "message": "refactor(client): minimize the amount of instantiated code (#2391)\n\n* Halve the amount of conn_task instantiations\r\n* Extract non-generic parts of client::handshake\r\n* Extract the non-generic parts of call_async",
+          "timestamp": "2021-01-12T16:18:22-08:00",
+          "tree_id": "56d1582e4c7f3e995ff4cba0fa9bf34cc857408d",
+          "url": "https://github.com/hyperium/hyper/commit/a15f3f7f0f536c74d51636bbc00f6b5ec110472b"
+        },
+        "date": 1610497247631,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 48603,
+            "range": "± 2381",
             "unit": "ns/iter"
           }
         ]
