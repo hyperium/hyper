d882b8ee351789c39406c6cb220bf81232ed9d25
github-action-benchmark <github@users.noreply.github.com>
2021-01-20 23:12:52 +0000

    add pipeline (cargo) benchmark result for 1928682b33f98244435ba6d574677546205a15ec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611184358331,
+  "lastUpdate": 1611184372022,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -2521,6 +2521,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 53011,
             "range": "± 5625",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "1928682b33f98244435ba6d574677546205a15ec",
+          "message": "feat(ffi): add HYPERE_INVALID_PEER_MESSAGE error code for parse errors",
+          "timestamp": "2021-01-20T15:10:19-08:00",
+          "tree_id": "e87d140b4320ce0f482aa7ee06ddeb97a4822f19",
+          "url": "https://github.com/hyperium/hyper/commit/1928682b33f98244435ba6d574677546205a15ec"
+        },
+        "date": 1611184370564,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 59284,
+            "range": "± 18434",
             "unit": "ns/iter"
           }
         ]
