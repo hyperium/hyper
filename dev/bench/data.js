e274fc8166d30e031ac34aa39edfc9397756dba0
github-action-benchmark <github@users.noreply.github.com>
2021-02-09 21:34:26 +0000

    add connect (cargo) benchmark result for 42587059e6175735b1a8656c5ddbff0edb19294c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612626077293,
+  "lastUpdate": 1612906466672,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -1559,6 +1559,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 62223,
             "range": "± 9341",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@arnavion.dev",
+            "name": "Arnavion",
+            "username": "Arnavion"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "42587059e6175735b1a8656c5ddbff0edb19294c",
+          "message": "docs(server): use cfg_feature! on hyper::server::conn::tcp instead of #[cfg]\n\nThis is required to surface the required feature (`tcp`) in the generated docs\nfor `hyper::server::conn::{AddrIncoming, AddrStream}`. Before this change,\ntheir docs only mentioned the features needed for the `hyper::server::conn` mod\nitself.\n\nFixes #2425",
+          "timestamp": "2021-02-09T13:31:41-08:00",
+          "tree_id": "f50609d1ee3a1c9696d0e6954a6ec2e68494cfba",
+          "url": "https://github.com/hyperium/hyper/commit/42587059e6175735b1a8656c5ddbff0edb19294c"
+        },
+        "date": 1612906465242,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 72941,
+            "range": "± 11762",
             "unit": "ns/iter"
           }
         ]
