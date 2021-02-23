5db68ff07b26c9537bd7685d1317726f66c6b4cf
github-action-benchmark <github@users.noreply.github.com>
2021-02-23 00:14:49 +0000

    add connect (cargo) benchmark result for a60280873bbfe5c2a2806c88891bf91e3e4c3882

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614022745096,
+  "lastUpdate": 1614039289689,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -1679,6 +1679,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 48833,
             "range": "± 2218",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cfir16@gmail.com",
+            "name": "CfirTsabari",
+            "username": "CfirTsabari"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a60280873bbfe5c2a2806c88891bf91e3e4c3882",
+          "message": "refactor(ffi): Removed need for cbindgen type renames (#2442)\n\nFixes hyperium/hyper#2428",
+          "timestamp": "2021-02-22T16:12:06-08:00",
+          "tree_id": "9f554a19f8d87074aa036b33399d42eed1a3f3ce",
+          "url": "https://github.com/hyperium/hyper/commit/a60280873bbfe5c2a2806c88891bf91e3e4c3882"
+        },
+        "date": 1614039288229,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 51930,
+            "range": "± 2781",
             "unit": "ns/iter"
           }
         ]
