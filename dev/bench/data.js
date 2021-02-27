c2e1daa50f3eac6e46caabb9e5ceebe2207edd16
github-action-benchmark <github@users.noreply.github.com>
2021-02-27 03:03:22 +0000

    add pipeline (cargo) benchmark result for f162ca2f2fd14681e11dd8b9ba8d1469b2b9271b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614039833190,
+  "lastUpdate": 1614395002106,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -3451,6 +3451,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 66023,
             "range": "± 14528",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f162ca2f2fd14681e11dd8b9ba8d1469b2b9271b",
+          "message": "docs(ffi): generate FFI documentation (#2447)",
+          "timestamp": "2021-02-26T19:00:37-08:00",
+          "tree_id": "6fb822dc3e7c899c8cfbd23348c87583879f465a",
+          "url": "https://github.com/hyperium/hyper/commit/f162ca2f2fd14681e11dd8b9ba8d1469b2b9271b"
+        },
+        "date": 1614395000589,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 57374,
+            "range": "± 4228",
             "unit": "ns/iter"
           }
         ]
