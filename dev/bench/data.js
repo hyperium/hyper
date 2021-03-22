22ac82b604bfc2b37c5a7a73287a8a5e62fa4c26
github-action-benchmark <github@users.noreply.github.com>
2021-03-22 22:19:43 +0000

    add pipeline (cargo) benchmark result for 41f99578a53845e5e0bb999c101bef8307e1ce5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616451567962,
+  "lastUpdate": 1616451583757,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -3841,6 +3841,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 54871,
             "range": "± 4548",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomasdezeeuw@gmail.com",
+            "name": "Thomas de Zeeuw",
+            "username": "Thomasdezeeuw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "41f99578a53845e5e0bb999c101bef8307e1ce5f",
+          "message": "refactor(dependencies): update to socket2 v0.4.0 (#2472)",
+          "timestamp": "2021-03-22T15:16:31-07:00",
+          "tree_id": "bd982e385979d9a9b4441522a8e54b095729bb2e",
+          "url": "https://github.com/hyperium/hyper/commit/41f99578a53845e5e0bb999c101bef8307e1ce5f"
+        },
+        "date": 1616451582317,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 67849,
+            "range": "± 32642",
             "unit": "ns/iter"
           }
         ]
