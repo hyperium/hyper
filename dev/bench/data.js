bb5a73fed08ebf35ba055897996e7bce048165a3
github-action-benchmark <github@users.noreply.github.com>
2021-03-25 20:36:47 +0000

    add connect (cargo) benchmark result for 51ed71b0a6e1d90d324f87151434334bab6fb837

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616451759407,
+  "lastUpdate": 1616704607010,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -1949,6 +1949,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 62460,
             "range": "± 7893",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vagelis.prokopiou@gmail.com",
+            "name": "Vagelis Prokopiou",
+            "username": "Vagelis-Prokopiou"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51ed71b0a6e1d90d324f87151434334bab6fb837",
+          "message": "docs(client): use Method::POST to match the example in https://hyper.rs/guides/client/advanced/ (#2479)",
+          "timestamp": "2021-03-25T13:34:03-07:00",
+          "tree_id": "908a1fb4a47378d16439458035a25a2dd41c1516",
+          "url": "https://github.com/hyperium/hyper/commit/51ed71b0a6e1d90d324f87151434334bab6fb837"
+        },
+        "date": 1616704605487,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 46910,
+            "range": "± 2399",
             "unit": "ns/iter"
           }
         ]
