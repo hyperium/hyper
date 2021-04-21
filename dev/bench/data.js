7e5f0943cc0d9bba5e8a2b77cfa0df4241893bcf
github-action-benchmark <github@users.noreply.github.com>
2021-04-21 23:24:24 +0000

    add pipeline (cargo) benchmark result for 7f69d8f94ccf258190f40585cd668e986b0829e4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619047450039,
+  "lastUpdate": 1619047464745,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -4681,6 +4681,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 59512,
             "range": "± 14791",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "7f69d8f94ccf258190f40585cd668e986b0829e4",
+          "message": "v0.14.6",
+          "timestamp": "2021-04-21T16:19:31-07:00",
+          "tree_id": "415b1e099ad45a3af99415ebb44197e9ef4dc0f6",
+          "url": "https://github.com/hyperium/hyper/commit/7f69d8f94ccf258190f40585cd668e986b0829e4"
+        },
+        "date": 1619047462798,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 65724,
+            "range": "± 8905",
             "unit": "ns/iter"
           }
         ]
