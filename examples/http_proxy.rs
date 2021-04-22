4fd6c4cb0b58bb0831ae0f876d858aba1588d0e3
Anthony Ramine <nox@nox.paris>
2021-04-22 09:13:51 +0200

    feat(server): implement forgotten settings for case preserving

--- examples/http_proxy.rs
+++ examples/http_proxy.rs
@@ -23,14 +23,21 @@ type HttpClient = Client<hyper::client::HttpConnector>;
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8100));
-    let client = HttpClient::new();
+
+    let client = Client::builder()
+        .http1_title_case_headers(true)
+        .http1_preserve_header_case(true)
+        .build_http();
 
     let make_service = make_service_fn(move |_| {
         let client = client.clone();
         async move { Ok::<_, Infallible>(service_fn(move |req| proxy(client.clone(), req))) }
     });
 
-    let server = Server::bind(&addr).serve(make_service);
+    let server = Server::bind(&addr)
+        .http1_preserve_header_case(true)
+        .http1_title_case_headers(true)
+        .serve(make_service);
 
     println!("Listening on http://{}", addr);
 
