6778b3ad39301d64b21640c1f5beab4b9cd97236
github-action-benchmark <github@users.noreply.github.com>
2021-05-13 00:54:33 +0000

    add pipeline (cargo) benchmark result for ccba59fb1b592dfdfca4b870e0922e5ba8244825

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620867263499,
+  "lastUpdate": 1620867273838,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -5101,6 +5101,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 71149,
             "range": "± 18930",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ccba59fb1b592dfdfca4b870e0922e5ba8244825",
+          "message": "docs(common): remove favicon doc from sync_wrapper module (#2548)",
+          "timestamp": "2021-05-13T00:51:45Z",
+          "tree_id": "12711abbabe5166eda6cb05bd0736aa4e80efea1",
+          "url": "https://github.com/hyperium/hyper/commit/ccba59fb1b592dfdfca4b870e0922e5ba8244825"
+        },
+        "date": 1620867271867,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 64861,
+            "range": "± 18600",
             "unit": "ns/iter"
           }
         ]
