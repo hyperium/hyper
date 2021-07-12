cdd7bbf9451b4af7db723804539d1f3635cf5219
github-action-benchmark <github@users.noreply.github.com>
2021-07-12 22:46:13 +0000

    add connect (cargo) benchmark result for 25d18c0b74ccf9e51f986daa3b2b98c0109f827a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1625684027774,
+  "lastUpdate": 1626129973125,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -3149,6 +3149,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 46950,
             "range": "± 784",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "25d18c0b74ccf9e51f986daa3b2b98c0109f827a",
+          "message": "feat(ffi): add hyper_request_on_informational\n\nThis defines an extension type used in requests for the client that is\nused to setup a callback for receipt of informational (1xx) responses.\nThe type isn't currently public, and is only usable in the C API.",
+          "timestamp": "2021-07-12T15:43:58-07:00",
+          "tree_id": "da554dd5b853aebeb130f781735d452941b06e1e",
+          "url": "https://github.com/hyperium/hyper/commit/25d18c0b74ccf9e51f986daa3b2b98c0109f827a"
+        },
+        "date": 1626129971484,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 46048,
+            "range": "± 3856",
             "unit": "ns/iter"
           }
         ]
