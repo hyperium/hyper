5ba9a06ff20d19c056271b5639f4077a3a28f3e4
github-action-benchmark <github@users.noreply.github.com>
2021-10-04 20:37:46 +0000

    add connect (cargo) benchmark result for 6169db250c932dd012d391389826cd34833077b4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632265663627,
+  "lastUpdate": 1633379866169,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -4199,6 +4199,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 49400,
             "range": "± 3910",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nox@nox.paris",
+            "name": "Anthony Ramine",
+            "username": "nox"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "6169db250c932dd012d391389826cd34833077b4",
+          "message": "feat(h2): always include original h2 error on broken pipe",
+          "timestamp": "2021-10-04T13:35:07-07:00",
+          "tree_id": "e55c996e68297d08a6630e792941b468bd581612",
+          "url": "https://github.com/hyperium/hyper/commit/6169db250c932dd012d391389826cd34833077b4"
+        },
+        "date": 1633379863840,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 73655,
+            "range": "± 11628",
             "unit": "ns/iter"
           }
         ]
