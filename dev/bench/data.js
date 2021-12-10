9aa41336f7dae9709c1439cd936f8b245a8e6ee9
github-action-benchmark <github@users.noreply.github.com>
2021-12-10 01:06:51 +0000

    add connect (cargo) benchmark result for 9ee73231cc9c81155be6f33e94e4a1a756d46a3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639074204210,
+  "lastUpdate": 1639098411244,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "connect": [
@@ -4919,6 +4919,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 54168,
             "range": "± 4919",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "9ee73231cc9c81155be6f33e94e4a1a756d46a3f",
+          "message": "docs(dev): rename dev docs directory to 'docs'",
+          "timestamp": "2021-12-09T17:02:25-08:00",
+          "tree_id": "1c9814553cfc70eb2e384696f537a51796946789",
+          "url": "https://github.com/hyperium/hyper/commit/9ee73231cc9c81155be6f33e94e4a1a756d46a3f"
+        },
+        "date": 1639098408738,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 59215,
+            "range": "± 6371",
             "unit": "ns/iter"
           }
         ]
