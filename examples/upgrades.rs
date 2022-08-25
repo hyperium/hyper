6b297809ddcb7f10b713f7877018bc70c26543f9
Michael J Ward <ward.michael.j@gmail.com>
2022-08-24 22:52:18 -0400

    updates http2 version used in upgrades and web-api examples
    Switched per @seanmonstar's feedback in PR #2961
    https://github.com/hyperium/hyper/pull/2961

--- examples/upgrades.rs
+++ examples/upgrades.rs
@@ -95,7 +95,7 @@ async fn client_upgrade_request(addr: SocketAddr) -> Result<()> {
         .unwrap();
 
     let stream = TcpStream::connect(addr).await?;
-    let (mut sender, conn) = hyper::client::conn::http2::handshake(stream).await?;
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(stream).await?;
 
     tokio::task::spawn(async move {
         if let Err(err) = conn.await {
