95271ef8abb0cb79f2d44d78df6bf6e94d72e875
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-10-05 01:05:03 +0200

    feat(http2) provide h2 missing parameters
    Add the missing h2 Builder configuration parameters, this will provide
    more options to hyper's builders.
    
    Closes #2599

--- src/proto/h2/client.rs
+++ src/proto/h2/client.rs
@@ -38,18 +38,23 @@ const DEFAULT_CONN_WINDOW: u32 = 1024 * 1024 * 5; // 5mb
 const DEFAULT_STREAM_WINDOW: u32 = 1024 * 1024 * 2; // 2mb
 const DEFAULT_MAX_FRAME_SIZE: u32 = 1024 * 16; // 16kb
 const DEFAULT_MAX_SEND_BUF_SIZE: usize = 1024 * 1024; // 1mb
+const DEFAULT_RESET_STREAM_SECS: u64 = 30; // 30s
 
 #[derive(Clone, Debug)]
 pub(crate) struct Config {
     pub(crate) adaptive_window: bool,
     pub(crate) initial_conn_window_size: u32,
     pub(crate) initial_stream_window_size: u32,
+    pub(crate) initial_max_send_streams: usize,
     pub(crate) max_frame_size: u32,
+    pub(crate) max_header_list_size: Option<u32>,
     pub(crate) keep_alive_interval: Option<Duration>,
     pub(crate) keep_alive_timeout: Duration,
     pub(crate) keep_alive_while_idle: bool,
+    pub(crate) max_concurrent_streams: Option<u32>,
     pub(crate) max_concurrent_reset_streams: Option<usize>,
     pub(crate) max_send_buffer_size: usize,
+    pub(crate) reset_stream_duration: Duration,
 }
 
 impl Default for Config {
@@ -58,12 +63,16 @@ impl Default for Config {
             adaptive_window: false,
             initial_conn_window_size: DEFAULT_CONN_WINDOW,
             initial_stream_window_size: DEFAULT_STREAM_WINDOW,
+            initial_max_send_streams: usize::MAX,
             max_frame_size: DEFAULT_MAX_FRAME_SIZE,
+            max_header_list_size: None,
             keep_alive_interval: None,
             keep_alive_timeout: Duration::from_secs(20),
             keep_alive_while_idle: false,
+            max_concurrent_streams: None,
             max_concurrent_reset_streams: None,
             max_send_buffer_size: DEFAULT_MAX_SEND_BUF_SIZE,
+            reset_stream_duration: Duration::from_secs(DEFAULT_RESET_STREAM_SECS)
         }
     }
 }
@@ -73,9 +82,17 @@ fn new_builder(config: &Config) -> Builder {
     builder
         .initial_window_size(config.initial_stream_window_size)
         .initial_connection_window_size(config.initial_conn_window_size)
+        .initial_max_send_streams(config.initial_max_send_streams)
         .max_frame_size(config.max_frame_size)
         .max_send_buffer_size(config.max_send_buffer_size)
+        .reset_stream_duration(config.reset_stream_duration)
         .enable_push(false);
+    if let Some(max) = config.max_header_list_size {
+        builder.max_header_list_size(max);
+    }
+    if let Some(max) = config.max_concurrent_streams {
+        builder.max_concurrent_streams(max);
+    }
     if let Some(max) = config.max_concurrent_reset_streams {
         builder.max_concurrent_reset_streams(max);
     }
