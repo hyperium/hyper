3d2f3ce46eb4ea4b329197ad09f6569228f961e5
Andy Caldwell <andycaldwell@microsoft.com>
2022-12-14 18:26:52 +0000

    Fix up test for new Sleep API

--- benches/support/tokiort.rs
+++ benches/support/tokiort.rs
@@ -8,7 +8,6 @@ use std::{
 
 use futures_util::Future;
 use hyper::rt::{Sleep, Timer};
-use pin_project_lite::pin_project;
 
 #[derive(Clone)]
 /// An Executor that uses the tokio runtime.
@@ -31,15 +30,11 @@ pub struct TokioTimer;
 
 impl Timer for TokioTimer {
     fn sleep(&self, duration: Duration) -> Pin<Box<dyn Sleep>> {
-        Box::pin(TokioSleep {
-            inner: tokio::time::sleep(duration),
-        })
+        Box::pin(tokio::time::sleep(duration))
     }
 
     fn sleep_until(&self, deadline: Instant) -> Pin<Box<dyn Sleep>> {
-        Box::pin(TokioSleep {
-            inner: tokio::time::sleep_until(deadline.into()),
-        })
+        Box::pin(tokio::time::sleep_until(deadline.into()))
     }
 }
 
@@ -57,25 +52,3 @@ where
         self.inner.as_mut().poll(context)
     }
 }
-
-// Use TokioSleep to get tokio::time::Sleep to implement Unpin.
-// see https://docs.rs/tokio/latest/tokio/time/struct.Sleep.html
-pin_project! {
-    pub(crate) struct TokioSleep {
-        #[pin]
-        pub(crate) inner: tokio::time::Sleep,
-    }
-}
-
-impl Future for TokioSleep {
-    type Output = ();
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        self.project().inner.poll(cx)
-    }
-}
-
-// Use HasSleep to get tokio::time::Sleep to implement Unpin.
-// see https://docs.rs/tokio/latest/tokio/time/struct.Sleep.html
-
-impl Sleep for TokioSleep {}
