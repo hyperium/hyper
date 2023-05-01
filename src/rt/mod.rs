d4295eb976ef424939797f7e3f1e2d85aee8b224
emfax <6355781+emfax@users.noreply.github.com>
2023-05-01 23:31:22 +1000

    docs(rt): make purpose of `Executor` trait clearer (#3217)
    Make the purpose of `Executor` clearer by explaining why it is necessary.
    
    Co-authored-by: Matt Andrews <mattandrews@massey.com.au>

--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -15,7 +15,7 @@ use std::{
 
 /// An executor of futures.
 ///
-/// This trait should be implemented for any future.
+/// This trait allows Hyper to abstract over async runtimes. Implement this trait for your own type.
 ///
 /// # Example
 ///
