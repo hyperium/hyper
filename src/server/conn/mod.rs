733c5070d9599b93ac44319b379549d66379c72a
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-09-24 16:57:50 +0200

    docs(server): use hyphen in auto-connection

--- src/server/conn/mod.rs
+++ src/server/conn/mod.rs
@@ -8,7 +8,7 @@
 //! This module is split by HTTP version, providing a connection builder for
 //! each. They work similarly, but they each have specific options.
 //!
-//! If your server needs to support both versions, an auto connection builder is
+//! If your server needs to support both versions, an auto-connection builder is
 //! provided in the [`hyper-util`](https://github.com/hyperium/hyper-util/tree/master)
 //! crate. This builder wraps the HTTP/1 and HTTP/2 connection builders from this
 //! module, allowing you to set configuration for both. The builder will then check
