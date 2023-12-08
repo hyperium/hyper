61ba4dec18192a84bfe723455e920bcf23a438d3
Dax Huiberts <daxhuiberts@gmail.com>
2023-12-08 13:52:44 +0100

    refactor(common): Remove unused TokioTimeout

--- benches/support/tokiort.rs
+++ benches/support/tokiort.rs
@@ -49,21 +49,6 @@ impl Timer for TokioTimer {
     }
 }
 
-struct TokioTimeout<T> {
-    inner: Pin<Box<tokio::time::Timeout<T>>>,
-}
-
-impl<T> Future for TokioTimeout<T>
-where
-    T: Future,
-{
-    type Output = Result<T::Output, tokio::time::error::Elapsed>;
-
-    fn poll(mut self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Self::Output> {
-        self.inner.as_mut().poll(context)
-    }
-}
-
 // Use TokioSleep to get tokio::time::Sleep to implement Unpin.
 // see https://docs.rs/tokio/latest/tokio/time/struct.Sleep.html
 pin_project! {
