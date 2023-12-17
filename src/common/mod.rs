21aa2f1c69e1cc329b5787122c25596e0fa5e3ed
tottoto <tottotodev@gmail.com>
2023-12-17 22:43:43 +0900

    refactor(common): move common feature cfg in common::task to common::mod (#3492)

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -16,6 +16,7 @@ pub(crate) mod buf;
 #[cfg(all(feature = "server", any(feature = "http1", feature = "http2")))]
 pub(crate) mod date;
 pub(crate) mod io;
+#[cfg(all(any(feature = "client", feature = "server"), feature = "http1"))]
 pub(crate) mod task;
 #[cfg(any(
     all(feature = "server", feature = "http1"),
