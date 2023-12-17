1381061ea2614f8776ccdbd6b29cf1a71e515179
tottoto <tottotodev@gmail.com>
2023-12-17 15:53:06 +0900

    refactor(common): move common feature cfg in common::task to common::mod

--- src/common/task.rs
+++ src/common/task.rs
@@ -1,10 +1,8 @@
-#[cfg(all(any(feature = "client", feature = "server"), feature = "http1"))]
 use std::task::{Context, Poll};
 
 /// A function to help "yield" a future, such that it is re-scheduled immediately.
 ///
 /// Useful for spin counts, so a future doesn't hog too much time.
-#[cfg(all(any(feature = "client", feature = "server"), feature = "http1"))]
 pub(crate) fn yield_now(cx: &mut Context<'_>) -> Poll<std::convert::Infallible> {
     cx.waker().wake_by_ref();
     Poll::Pending
