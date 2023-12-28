7dc583e3d8325a6bf6b23dc2f8dac7f9b6f3d8a2
Daniel Gallups <daniel.gallups@adversarial.com>
2023-12-28 18:07:17 -0500

    feat(server|http2): add `max_pending_accept_reset_streams` back to `Config`
    In places where required, the `max_pending_accept_reset_streams` option was reimplemented, as in PR #3201
    
    May close #3461

--- src/server/conn/http2.rs
+++ src/server/conn/http2.rs
@@ -120,6 +120,17 @@ impl<E> Builder<E> {
         }
     }
 
+    /// Configures the maximum number of pending reset streams allowed before a GOAWAY will be sent.
+    ///
+    /// This will default to the default value set by the [`h2` crate](https://crates.io/crates/h2).
+    /// As of v0.4.0, it is 20.
+    ///
+    /// See <https://github.com/hyperium/hyper/issues/2877> for more information.
+    pub fn max_pending_accept_reset_streams(&mut self, max: impl Into<Option<usize>>) -> &mut Self {
+        self.h2_builder.max_pending_accept_reset_streams = max.into();
+        self
+    }
+
     /// Sets the [`SETTINGS_INITIAL_WINDOW_SIZE`][spec] option for HTTP2
     /// stream-level flow control.
     ///
