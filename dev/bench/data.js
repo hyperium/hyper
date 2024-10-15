06012565c425ac8fe729e4751c0afe743ce3a876
github-action-benchmark <github@users.noreply.github.com>
2024-10-15 15:28:53 +0000

    add pipeline (cargo) benchmark result for 3900a2381b96a7e7f608a5e031b3e90ddcdfcd74

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728671422936,
+  "lastUpdate": 1729006133540,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "pipeline": [
@@ -8759,6 +8759,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 52716,
             "range": "± 9717.41",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "3900a2381b96a7e7f608a5e031b3e90ddcdfcd74",
+          "message": "perf(http1): improve parsing of sequentially partial messages\n\nIf request headers are received in incremental partial chunks, hyper\nwould restart parsing each time. This is because the HTTP/1 parser is\nstateless, since the most common case is a full message and stateless\nparses faster.\n\nHowever, if continuing to receive more partial chunks of the request,\neach subsequent full parse is slower and slower. Since partial parses is\nless common, we can store a little bit of state to improve performance\nin general.\n\nNow, if a partial request is received, hyper will check for the end of\nthe message quickly, and if not found, simply save the length to allow\nthe next partial chunk to start its search from there. Only once the end\nis found will a fill parse happen.\n\nReported-by: Datong Sun <datong.sun@konghq.com>",
+          "timestamp": "2024-10-15T08:28:03-07:00",
+          "tree_id": "c71c7e4fb8dfcac629682e8a830453d4141dea4c",
+          "url": "https://github.com/hyperium/hyper/commit/3900a2381b96a7e7f608a5e031b3e90ddcdfcd74"
+        },
+        "date": 1729006131306,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 48263,
+            "range": "± 9399.11",
             "unit": "ns/iter"
           }
         ]
