ede24d2714ebce9113b88c94ec8e7104b637de30
katelyn martin <me+cratelyn@katelyn.world>
2025-04-04 14:54:23 -0400

    docs(service): add `HttpService` documentation (#3869)
    this commit introduces some additional documentation to the
    `HttpService` trait, and the `Service` trait.
    
    notably, this commit introduces some intradoc links, so that rustdoc
    will render links to types like `http::Request` and `http::Response`, or
    to the `Body` trait.
    
    additionally, mention of `hyper-util` is added to the `Service` trait,
    to direct users to the glue implementations that they will likely need
    to interact with e.g. `tower`.
    
    Signed-off-by: katelyn martin <me+cratelyn@katelyn.world>

--- src/service/http.rs
+++ src/service/http.rs
@@ -5,19 +5,32 @@ use crate::body::Body;
 use crate::service::service::Service;
 use crate::{Request, Response};
 
-/// An asynchronous function from `Request` to `Response`.
+/// An asynchronous function from [`Request`] to [`Response`].
+///
+/// This is a *sealed* trait, meaning that it can not be implemented directly. Rather, it is an
+/// alias for [`Service`]s that accept a [`Request`] and return a [`Future`] that resolves to a
+/// [`Response`]. External callers should implement [`Service`] instead.
+///
+/// Rather than being generic over the request and response, this trait is generic across the
+/// request [`Body`] and response [`Body`].
+///
+/// See the crate-level [`service`][crate::service] documentation for more information.
+///
+/// See [`Service`] for more information.
 pub trait HttpService<ReqBody>: sealed::Sealed<ReqBody> {
-    /// The `Body` body of the `http::Response`.
+    /// The [`Body`] body of the [`Response`].
     type ResBody: Body;
 
-    /// The error type that can occur within this `Service`.
+    /// The error type that can occur within this [`Service`].
     ///
-    /// Note: Returning an `Error` to a hyper server will cause the connection
-    /// to be abruptly aborted. In most cases, it is better to return a `Response`
-    /// with a 4xx or 5xx status code.
+    /// Note: Returning an `Error` to a hyper server, the behavior depends on the protocol. In
+    /// most cases, hyper will cause the connection to be abruptly aborted. In most cases, it is
+    /// better to return a `Response` with a 4xx or 5xx status code.
+    ///
+    /// See [`Service::Error`] for more information.
     type Error: Into<Box<dyn StdError + Send + Sync>>;
 
-    /// The `Future` returned by this `Service`.
+    /// The [`Future`] returned by this [`Service`].
     type Future: Future<Output = Result<Response<Self::ResBody>, Self::Error>>;
 
     #[doc(hidden)]
