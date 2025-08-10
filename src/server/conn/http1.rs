5e66519d2c32cc6fc308ebdc6921af5ad7d9e6fc
Ryan McCord <mccordryan00@gmail.com>
2025-08-10 16:40:19 -0500

    feat(http1): add `allow_multiple_spaces_in_request_line_delimiters` config method to h1 server builder

--- src/server/conn/http1.rs
+++ src/server/conn/http1.rs
@@ -276,6 +276,13 @@ impl Builder {
         self
     }
 
+    // Set whether multiple spaces are allowed as delimiters in request lines.
+    pub fn allow_multiple_spaces_in_request_line_delimiters(&mut self, enabled: bool) -> &mut Self {
+        self.h1_parser_config
+            .allow_multiple_spaces_in_request_line_delimiters(enabled);
+        self
+    }
+
     /// Set whether HTTP/1 connections will silently ignored malformed header lines.
     ///
     /// If this is enabled and a header line does not start with a valid header
