4c38fdeab1dfacdca78e005dea8c51d2938db626
Manish Mishra <mmishra@x.ai>
2026-03-31 14:18:42 +0100

    mark send_reset as client feature
    Co-authored-by: Sean McArthur <sean@seanmonstar.com>

--- src/proto/h2/mod.rs
+++ src/proto/h2/mod.rs
@@ -105,6 +105,7 @@ where
         }
     }
 
+    #[cfg(feature = "client")]
     fn send_reset(self: Pin<&mut Self>, reason: h2::Reason) {
         self.project().body_tx.send_reset(reason);
     }
