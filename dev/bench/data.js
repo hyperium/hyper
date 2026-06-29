6a02a15b151f575c8d86e144f72c043236115185
github-action-benchmark <github@users.noreply.github.com>
2026-06-29 16:10:26 +0000

    add end_to_end (cargo) benchmark result for e0d14d19a0a87962efe92acdaa029253be54a612

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782749291310,
+  "lastUpdate": 1782749425949,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "pipeline": [
@@ -63847,6 +63847,114 @@ window.BENCHMARK_DATA = {
             "name": "http2_parallel_x10_res_1mb",
             "value": 5619393,
             "range": "± 85869.04",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32043000+josetorrs@users.noreply.github.com",
+            "name": "Jose",
+            "username": "josetorrs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e0d14d19a0a87962efe92acdaa029253be54a612",
+          "message": "style(proto): explicitly allow `decimal_literal_representation` lint (#4117)\n\nFor readability purposes.\n\nFor instance, 65_535 is more readable than 0xFFFF, when the spec uses doesn't use hexadecimal in that case.",
+          "timestamp": "2026-06-29T12:07:30-04:00",
+          "tree_id": "82d40b222c10174132422c28cfb69e807020e91b",
+          "url": "https://github.com/hyperium/hyper/commit/e0d14d19a0a87962efe92acdaa029253be54a612"
+        },
+        "date": 1782749423309,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http1_consecutive_x1_both_100kb",
+            "value": 68532,
+            "range": "± 1754.11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_both_10mb",
+            "value": 4142891,
+            "range": "± 98785.14",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_empty",
+            "value": 20837,
+            "range": "± 214.87",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_req_10b",
+            "value": 22752,
+            "range": "± 796.99",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_empty",
+            "value": 31131,
+            "range": "± 3540.37",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_req_100kb",
+            "value": 101588,
+            "range": "± 2669.51",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_req_10b",
+            "value": 41000264,
+            "range": "± 42887.82",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_empty",
+            "value": 84196,
+            "range": "± 1423.83",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks",
+            "value": 32410382,
+            "range": "± 16529560.17",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks_adaptive_window",
+            "value": 7898656,
+            "range": "± 105446.68",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks_max_window",
+            "value": 7612411,
+            "range": "± 85314.27",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10mb",
+            "value": 51172433,
+            "range": "± 466077.64",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_res_10mb",
+            "value": 52595690,
+            "range": "± 448508.81",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_res_1mb",
+            "value": 5486699,
+            "range": "± 61158.26",
             "unit": "ns/iter"
           }
         ]
