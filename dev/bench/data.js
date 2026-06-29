85b3dc7afff7a80b7220cb6b7ae9c8da180afceb
github-action-benchmark <github@users.noreply.github.com>
2026-06-29 16:08:09 +0000

    add pipeline (cargo) benchmark result for e0d14d19a0a87962efe92acdaa029253be54a612

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782749171338,
+  "lastUpdate": 1782749289764,
   "repoUrl": "https://github.com/hyperium/hyper",
   "entries": {
     "pipeline": [
@@ -12569,6 +12569,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 53418,
             "range": "± 6774.61",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32043000+josetorrs@users.noreply.github.com",
+            "name": "Jose",
+            "username": "josetorrs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e0d14d19a0a87962efe92acdaa029253be54a612",
+          "message": "style(proto): explicitly allow `decimal_literal_representation` lint (#4117)\n\nFor readability purposes.\n\nFor instance, 65_535 is more readable than 0xFFFF, when the spec uses doesn't use hexadecimal in that case.",
+          "timestamp": "2026-06-29T12:07:30-04:00",
+          "tree_id": "82d40b222c10174132422c28cfb69e807020e91b",
+          "url": "https://github.com/hyperium/hyper/commit/e0d14d19a0a87962efe92acdaa029253be54a612"
+        },
+        "date": 1782749286635,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 38458,
+            "range": "± 5621.87",
             "unit": "ns/iter"
           }
         ]
